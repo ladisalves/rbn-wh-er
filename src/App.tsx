26e2ba4f3d4884db2d26364e1856bfdc2d667f74
Ladislav Stefan <ladislaves@gmail.com>
2021-11-30 23:54:33 +0100

    feat: app wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import './App.css'
+import styled from 'styled-components'
+import ExchangeRateList from './component/ExchangeRateList'
+import { ExchangeRate } from './types'
+import ConversionForm from './component/ConversionForm'
+
+const rates: ExchangeRate[] = [
+  { rate: 11.1, country: 'Australia', name: 'dollar', code: 'AUD', quantity: 1 },
+  { rate: 21.1, country: 'EMU', name: 'euro', code: 'EUR', quantity: 1 },
+]
+
+const AppWrapper = styled.div`
+  width: 800px;
+  margin: 45px auto;
+`
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <h1>Czech National Bank Exchange Rates</h1>
+      <ConversionForm rates={rates} />
+      <ExchangeRateList rates={rates} />
+    </AppWrapper>
   )
 }
 
