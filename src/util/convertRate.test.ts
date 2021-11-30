a682d1bf09a44c77f1be23d60a31d47db6b5b6ef
Ladislav Stefan <ladislaves@gmail.com>
2021-11-30 23:27:50 +0100

    feat: getRoundedAmount - amount rounded up to 4 decimals

--- src/util/convertRate.test.ts
+++ src/util/convertRate.test.ts
@@ -1,4 +1,4 @@
-import { convertRate } from './convertRate'
+import { convertRate, getRoundedAmount } from './convertRate'
 import { ExchangeRate } from '../types'
 
 const rates: ExchangeRate[] = [
@@ -6,7 +6,7 @@ const rates: ExchangeRate[] = [
   { rate: 18.801, country: 'Japan', name: 'yen', code: 'JPY', quantity: 100 },
 ]
 
-describe('Amount converstion', () => {
+describe('Amount conversion', () => {
   it('should convert amount to selected currency', () => {
     expect(convertRate(1000, 'AUD', rates)).toBe(62.2781)
     expect(convertRate(1000, 'JPY', rates)).toBe(5318.866)
@@ -14,5 +14,11 @@ describe('Amount converstion', () => {
 
   it('should throw an exception for unknown currency', () => {
     expect(() => convertRate(1000, 'XYZ', rates)).toThrow(new Error('Exchange rate not found'))
+  })
+
+  it('should round amount up to 4 decimals', () => {
+    expect(getRoundedAmount(1000)).toBe(1000.0)
+    expect(getRoundedAmount(1000.12)).toBe(1000.12)
+    expect(getRoundedAmount(1000.12345678)).toBe(1000.1235)
   })
 })
--- src/util/convertRate.ts
+++ src/util/convertRate.ts
@@ -9,5 +9,7 @@ export const convertRate = (amount: number, code: string, rates: ExchangeRate[])
 
   const rate = (amount / Math.max(1, exchangeRate.rate)) * Math.max(1, exchangeRate.quantity)
 
-  return Math.round(rate * 10000) / 10000
+  return getRoundedAmount(rate)
 }
+
+export const getRoundedAmount = (amount: number): number => Math.round(amount * 10000) / 10000
