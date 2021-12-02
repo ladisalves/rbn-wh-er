6d1150b727b4ba5aea02bbe589765e12fee277d8
Ladislav Stefan <ladislaves@gmail.com>
2021-12-02 17:17:47 +0100

    refactor: Loading, ErrorMessage, proxy usage

--- src/component/ExhangeRates.tsx
+++ src/component/ExhangeRates.tsx
@@ -4,26 +4,34 @@ import { ExchangeRate } from '../types'
 import ConversionForm from './ConversionForm'
 import ExchangeRateList from './ExchangeRateList'
 import { parseTextResponse } from '../util/parseTextResponse'
+import { Loading } from './Loading'
+import { ErrorMessage } from './ErrorMessage'
+import { getCorsProxyUrl } from '../util/corsProxy'
 
 const ExchangeRates: React.FC = () => {
   const { isLoading, error, data } = useQuery('cnb', async () => {
+    // TODO: build configuration for URL end-point
     const response = await fetch(
-      'https://cors-anywhere.herokuapp.com/www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt',
+      getCorsProxyUrl(
+        'https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt',
+      ),
     )
 
     return response.text()
   })
 
-  const rates = useMemo<ExchangeRate[]>(() => (data ? parseTextResponse(data) : []), [data])
-
-  console.log(rates)
+  const rates = useMemo<ExchangeRate[] | undefined>(() => (data ? parseTextResponse(data) : undefined), [data])
 
   return (
     <div>
-      {rates && <ConversionForm rates={rates} />}
-      {rates && <ExchangeRateList rates={rates} />}
-      {isLoading && <div>loading</div>}
-      {error && <div>error</div>}
+      {rates && !error && (
+        <>
+          <ConversionForm rates={rates} />
+          <ExchangeRateList rates={rates} />
+        </>
+      )}
+      {isLoading && <Loading />}
+      {error && <ErrorMessage />}
     </div>
   )
 }
